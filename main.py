0db7365f0f38fd7bb0c3ad3e9bb99a205e24b1a1
Joswin John <joswinn15@gmail.com>
2022-10-21 13:24:49 -0400

    init2

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
-
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
