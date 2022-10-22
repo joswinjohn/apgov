2de8c6885383a295ada6a96129d65f71225ce9cd
Joswin John <joswinn15@gmail.com>
2022-10-22 18:16:58 -0400

    jus for u murthi

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
-    $(".menu-icon").on("click", function() {
-          $("nav ul").toggleClass("showing");
-    });
+      let open = false;
+      $(".menu-icon").on("click", function() {
+            $("nav ul").toggleClass("showing");
+            open = !open;
+            if (open == false) {
+                  $("#vl").css('transition', '0.5s');
+            } else {
+                  $("#vl").css('transition', '2s');
+            }
+            $(".vl").toggleClass("showing");
+      });
 });
 
 // Scrolling Effect
@@ -15,3 +23,15 @@ $(window).on("scroll", function() {
           $('nav').removeClass('black');
     }
 })
+
+$('.slide').on('click', function(){
+      $('#fade-in').toggleClass('show');
+});
+  
+$('.spin').on('click', function(){
+      $('#spin-in').toggleClass('show2');
+});
+
+$('h1.spacing').on('mouseenter', function(){
+      $(this).toggleClass('spaced');
+});
