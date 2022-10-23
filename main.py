1e2b4b2af70cc7d49b6ef35c851295edc2c39ecc
Joswin John <joswinn15@gmail.com>
2022-10-23 11:19:15 -0400

    templates for all 9 cases created

--- main.py
+++ main.py
@@ -8,5 +8,41 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/engel-vitale')
+def engel():
+    return render_template('engel.html')
+
+@app.route('/wisconsin-yoder')
+def wisconsin():
+    return render_template('wisconsin.html')
+
+@app.route('/tinker-desmoines')
+def tinker():
+    return render_template('tinker.html')
+
+@app.route('/schenck-us')
+def schenck():
+    return render_template('schenck.html')
+
+@app.route('/nytc-us')
+def nytc():
+    return render_template('nytc.html')
+
+@app.route('/mcdonald-chicago')
+def mcdonald():
+    return render_template('mcdonald.html')
+
+@app.route('/gideon-wainwright')
+def gideon():
+    return render_template('gideon.html')
+
+@app.route('/roe-wade')
+def roe():
+    return render_template('roe.html')
+
+@app.route('/brown-board')
+def brown():
+    return render_template('brown.html')
+
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
--- static/js/main.js
+++ static/js/main.js
@@ -10,43 +10,42 @@ var sections_loaded = 0;
 
 $(window).on("scroll", function() {
       var scrollPos = $(document).scrollTop();
-
-
+      var vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
       var objects = document.getElementsByClassName('fade-in');
 
-      if ((objects[0].getBoundingClientRect().top <= 700) && (sections_loaded == 0)) {
+      if ((objects[0].getBoundingClientRect().top <= vh/1.3) && (sections_loaded == 0)) {
             fade_in(0);
             sections_loaded++;
             console.log("sections_loaded: " + sections_loaded);
-      } else if ((objects[1].getBoundingClientRect().top <= 700) && (sections_loaded == 1)) {
+      } else if ((objects[1].getBoundingClientRect().top <= vh/1.3) && (sections_loaded == 1)) {
             fade_in(1);
             sections_loaded++;
             console.log("sections_loaded: " + sections_loaded);
-      } else if ((objects[2].getBoundingClientRect().top <= 700) && (sections_loaded == 2)) {
+      } else if ((objects[2].getBoundingClientRect().top <= vh/1.3) && (sections_loaded == 2)) {
             fade_in(2);
             sections_loaded++;
             console.log("sections_loaded: " + sections_loaded);
-      } else if ((objects[3].getBoundingClientRect().top <= 700) && (sections_loaded == 3)) {
+      } else if ((objects[3].getBoundingClientRect().top <= vh/1.3) && (sections_loaded == 3)) {
             fade_in(3);
             sections_loaded++;
             console.log("sections_loaded: " + sections_loaded);
-      } else if ((objects[4].getBoundingClientRect().top <= 700) && (sections_loaded == 4)) {
+      } else if ((objects[4].getBoundingClientRect().top <= vh/1.3) && (sections_loaded == 4)) {
             fade_in(4);
             sections_loaded++;
             console.log("sections_loaded: " + sections_loaded);
-      } else if ((objects[5].getBoundingClientRect().top <= 700) && (sections_loaded == 5)) {
+      } else if ((objects[5].getBoundingClientRect().top <= vh/1.3) && (sections_loaded == 5)) {
             fade_in(5);
             sections_loaded++;
             console.log("sections_loaded: " + sections_loaded);
-      } else if ((objects[6].getBoundingClientRect().top <= 700) && (sections_loaded == 6)) {
+      } else if ((objects[6].getBoundingClientRect().top <= vh/1.3) && (sections_loaded == 6)) {
             fade_in(6);
             sections_loaded++;
             console.log("sections_loaded: " + sections_loaded);
-      } else if ((objects[7].getBoundingClientRect().top <= 700) && (sections_loaded == 7)) {
+      } else if ((objects[7].getBoundingClientRect().top <= vh/1.3) && (sections_loaded == 7)) {
             fade_in(7);
             sections_loaded++;
             console.log("sections_loaded: " + sections_loaded);
-      } else if ((objects[8].getBoundingClientRect().top <= 700) && (sections_loaded == 8)) {
+      } else if ((objects[8].getBoundingClientRect().top <= vh/1.3) && (sections_loaded == 8)) {
             fade_in(8);
             sections_loaded++;
             console.log("sections_loaded: " + sections_loaded);
