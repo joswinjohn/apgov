79d6d00a8f7a56835bc4f86a70b6a5782f701421
Joswin John <joswinn15@gmail.com>
2022-10-22 23:09:23 -0400

    navigation basic functionality

--- static/js/main.js
+++ static/js/main.js
@@ -2,36 +2,62 @@ $(document).ready(function() {
       let open = false;
       $(".menu-icon").on("click", function() {
             $("nav ul").toggleClass("showing");
-            open = !open;
-            if (open == false) {
-                  $("#vl").css('transition', '0.5s');
-            } else {
-                  $("#vl").css('transition', '2s');
-            }
-            $(".vl").toggleClass("showing");
       });
 });
 
 // Scrolling Effect
+var sections_loaded = 0;
 
 $(window).on("scroll", function() {
-    if($(window).scrollTop()) {
-          $('nav').addClass('black');
-    }
+      var scrollPos = $(document).scrollTop();
 
-    else {
-          $('nav').removeClass('black');
-    }
+
+      var objects = document.getElementsByClassName('fade-in');
+
+      if ((objects[0].getBoundingClientRect().top <= 700) && (sections_loaded == 0)) {
+            fade_in(0);
+            sections_loaded++;
+            console.log("sections_loaded: " + sections_loaded);
+      } else if ((objects[1].getBoundingClientRect().top <= 700) && (sections_loaded == 1)) {
+            fade_in(1);
+            sections_loaded++;
+            console.log("sections_loaded: " + sections_loaded);
+      } else if ((objects[2].getBoundingClientRect().top <= 700) && (sections_loaded == 2)) {
+            fade_in(2);
+            sections_loaded++;
+            console.log("sections_loaded: " + sections_loaded);
+      } else if ((objects[3].getBoundingClientRect().top <= 700) && (sections_loaded == 3)) {
+            fade_in(3);
+            sections_loaded++;
+            console.log("sections_loaded: " + sections_loaded);
+      } else if ((objects[4].getBoundingClientRect().top <= 700) && (sections_loaded == 4)) {
+            fade_in(4);
+            sections_loaded++;
+            console.log("sections_loaded: " + sections_loaded);
+      } else if ((objects[5].getBoundingClientRect().top <= 700) && (sections_loaded == 5)) {
+            fade_in(5);
+            sections_loaded++;
+            console.log("sections_loaded: " + sections_loaded);
+      } else if ((objects[6].getBoundingClientRect().top <= 700) && (sections_loaded == 6)) {
+            fade_in(6);
+            sections_loaded++;
+            console.log("sections_loaded: " + sections_loaded);
+      } else if ((objects[7].getBoundingClientRect().top <= 700) && (sections_loaded == 7)) {
+            fade_in(7);
+            sections_loaded++;
+            console.log("sections_loaded: " + sections_loaded);
+      } else if ((objects[8].getBoundingClientRect().top <= 700) && (sections_loaded == 8)) {
+            fade_in(8);
+            sections_loaded++;
+            console.log("sections_loaded: " + sections_loaded);
+      }
 })
 
-$('.slide').on('click', function(){
-      $('#fade-in').toggleClass('show');
-});
-  
-$('.spin').on('click', function(){
-      $('#spin-in').toggleClass('show2');
-});
 
-$('h1.spacing').on('mouseenter', function(){
-      $(this).toggleClass('spaced');
-});
+
+function fade_in(index) {
+      var objects = $(".fade-in");
+      console.log(objects);
+      objects.eq(index).toggleClass('show');
+}
+
